8939fb2fc07aead1bcca0bcae5aa0b8027f2b912
lissella <69310460+lissella@users.noreply.github.com>
2021-02-02 10:53:05 +0700

    Add files via upload

---
+++ week12/condition201.js
@@ -0,0 +1,6 @@
+const n = 5;
+const m = 2
+for(let i = 1;i<=n;i++)
+{
+    let x = console.log(m + ' x ' + i + ' = ' + i*m)
+}
---
+++ week12/condition202.js
@@ -0,0 +1,7 @@
+let i =1,
+while(i<3)
+{
+    //console.log('I Love Earth')
+    console.log('1 x' + i + ' = ' + i*1 )
+    i++; //i = i+1
+}
